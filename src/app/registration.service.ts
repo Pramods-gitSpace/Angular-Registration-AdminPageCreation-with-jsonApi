a9d7a85f16d7e40d53761ee4deae772ab7352362
Pramod Parupati <141019704+Pramods-gitSpace@users.noreply.github.com>
2023-08-02 22:34:11 +0530

    Update registration.service.ts

--- src/app/registration.service.ts
+++ src/app/registration.service.ts
@@ -17,40 +17,5 @@ export class RegistrationService {
     return this.httpClient.get("http://localhost:3000/userList");
   }
 
-  userList=[
-    {
-      'id':1,
-      'name':"pramod",
-      'email':'pramod@Wipro.com',
-      'reason':'have some issuedd jhhhhjgerguyugyuhdsncjn hhgghdfeyfefhuef',
-      'location':['hyderabad','chennai'],
-      'date':'2017-06-07'
-    },
-    {
-      'id':2,
-      'name':"vinod",
-      'email':'vinod@Wipro.com',
-      'reason':'have some issuedd jhhhhjgerguyugyuhdsncjn hhgghdfeyfefhuef',
-      'location':['hyderabad','chennai','pune'],
-      'date':'2017-06-07'
-    },
-    {
-      'id':3,
-      'name':"sbghbhbhc",
-      'email':'cmmkmkcjkjc@Wipro.com',
-      'reason':'have some issuedd jhhhhjgerguyugyuhdsncjn hhgghdfeyfefhuef',
-      'location':['hyderabad'],
-      'date':'2017-06-07'
-    },
-    {
-      'id':4,
-      'name':"vamshi",
-      'email':'vamshi@Wipro.com',
-      'reason':'have some issuedd jhhhhjgerguyugyuhdsncjn hhgghdfeyfefhuef',
-      'location':['hyderabad','chennai'],
-      'date':'2017-06-07'
-    }
-  ]
-
-
+  
 }
